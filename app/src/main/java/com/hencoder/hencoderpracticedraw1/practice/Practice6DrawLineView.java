9aa238257db0b7e75852b24f5e1900e46cdbfde2
liukangqi <330620143@qq.com>
2017-11-25 16:20:27 +0800

    练习drawLine方法

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
@@ -2,28 +2,41 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice6DrawLineView extends View {
+    private Paint paint;
 
     public Practice6DrawLineView(Context context) {
         super(context);
+        initPaint();
     }
 
     public Practice6DrawLineView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initPaint();
     }
 
     public Practice6DrawLineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initPaint();
     }
 
+    private void initPaint(){
+        paint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawLine() 方法画直线
+        canvas.drawLine(200,200,500,600,paint);
     }
 }
