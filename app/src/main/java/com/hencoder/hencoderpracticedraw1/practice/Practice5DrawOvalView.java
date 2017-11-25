99f967ee045a1c608d3a1ca2b4a92d60a3939767
liukangqi <330620143@qq.com>
2017-11-25 16:15:31 +0800

    练习drawOval方法

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
@@ -2,22 +2,37 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice5DrawOvalView extends View {
+    private Paint paint;
+    private RectF rect;
 
     public Practice5DrawOvalView(Context context) {
         super(context);
+        initPaint();
     }
 
     public Practice5DrawOvalView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initPaint();
     }
 
     public Practice5DrawOvalView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    private void initPaint() {
+        paint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+        rect=new RectF(50,50,350,200);
     }
 
     @Override
@@ -25,5 +40,6 @@ public class Practice5DrawOvalView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawOval() 方法画椭圆
+        canvas.drawOval(rect,paint);
     }
 }
